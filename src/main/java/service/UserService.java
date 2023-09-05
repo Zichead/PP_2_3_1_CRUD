0a634fcbc863a5aa3e3adbb81cf433c69ba31531
Zichead <nedomexo@gmail.com>
2023-09-05 22:21:27 +0300

    The Service layer has been created
    with methods for getting users and changing them and creating and using ip from the Dao layer

---
+++ src/main/java/service/UserService.java
@@ -0,0 +1,16 @@
+package service;
+
+import model.User;
+
+import java.util.List;
+
+public interface UserService {
+    void add(User user);
+
+    User findUserToID(Long id);
+
+    void deleteUser (Long id);
+
+    void updateUser (User user);
+    List<User> getUserList();
+}
